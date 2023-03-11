f4ded7f6fb292dd0e25cc6ec7a689ccc3eb20e34
Иннокентий Довлатов <unnokenti@gmail.com>
2023-03-11 10:43:53 +0300

    Выполнение задания 4.15 (Исправление ошибок)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 //task1
-/*
-const from = 0;
-const before = 10;
-const res = returnNumberFrom(from,before);
-console.log('Число из диапазона ' + from + ' : ' + before + ' равно = '+ res);
-*/
 
 function returnNumberFrom(a, b){
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
@@ -14,12 +8,7 @@ function returnNumberFrom(a, b){
 }
 
 //task2
-/*
-const line = 'Строка';
-const mLenght = 5;
-const answer = checkStringLength(line,mLenght);
-console.log(answer);
-*/
+
 function checkStringLength(string, maxLenght){
   return string.length <= maxLenght;
 }
@@ -34,8 +23,6 @@ for (let i = 1; i <= array.length; i++){
   const element = generatePhoto(i,i);
   array[i-1] = element;
 }
-
-//array.forEach(element => console.log(element));
 
 function generatePhoto(id, url){
   const descriptions = ['Поползень','Гастеруптиида','Лягушкорот','Квакша','Игуанодонт'];
