75d13084c70376ccc5f33ed3589e9534212714aa
Иннокентий Довлатов <unnokenti@gmail.com>
2023-03-24 18:32:51 +0300

    небольшая доработка кода.

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ const specifiedNumber = getNumber(25);
 const array = new Array(specifiedNumber);
 
 for (let i = 1; i <= array.length; i++){
-  const element = generatePhoto(i,i);
-  array[i-1] = element;
+  const element = generatePhoto(i , i);
+  array[i - 1] = element;
 }
 
 
