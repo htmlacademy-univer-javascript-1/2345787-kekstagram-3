570da749df12b52c41621fbd933c1e1a50abe285
Keks <keks@htmlacademy.ru>
2023-04-01 10:58:31 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/draw.js
@@ -0,0 +1,19 @@
+
+function drawPictures(array){
+  const fragment = document.createDocumentFragment();
+  const template = document.querySelector('#picture');
+
+  for(let i = 0; i < array.length; i++){
+    const picture = template.content.cloneNode(true);
+    const randomPicture = array[i];
+    picture.querySelector('.picture__img').src = randomPicture.url;
+    picture.querySelector('.picture__likes').textContent = randomPicture.likes;
+    picture.querySelector('.picture__comments').textContent = randomPicture.comments;
+    fragment.appendChild(picture);
+  }
+
+  const photoBlock = document.querySelector('.pictures');
+  photoBlock.appendChild(fragment);
+}
+
+export {drawPictures};
---
+++ 9/js/main.js
@@ -0,0 +1,7 @@
+import {generateArrayOfPhotos} from './data.js';
+import {drawPictures} from './draw.js';
+
+const array = generateArrayOfPhotos();
+drawPictures(array);
+
+//Задание 7 часть 2
